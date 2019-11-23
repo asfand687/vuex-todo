afafea9cb9c6bd1bf4abb9b70b0cfddd43e0342b
asfand687 <asfandyar687@gmail.com>
2019-11-23 16:23:48 +0500

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,43 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <img alt="Vue logo" src="./assets/logo.png" class="logo" />
+    <todo-list msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoList from "./components/TodoList";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    TodoList
   }
-}
+};
 </script>
 
-<style>
+<style lang="scss">
+* {
+  box-sizing: border-box;
+}
+
+.container {
+  max-width: 600px;
+  margin: 0 auto;
+}
+
+.logo {
+  display: block;
+  margin: 20px auto;
+  height: 75px;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  font-size: 24px;
 }
 </style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,260 @@
+<template>
+  <div>
+    <input
+      type="text"
+      class="todo-input"
+      placeholder="What needs to be done"
+      v-model="newTodo"
+      @keyup.enter="addTodo"
+    />
+    <div v-for="(todo, index) in todosFiltered" :key="todo.id" class="todo-item">
+      <div class="todo-item-left">
+        <input type="checkbox" v-model="todo.completed" @change="doneEdit(todo)" />
+        <div
+          v-if="!todo.editing"
+          class="todo-item-label"
+          :class="{ completed: todo.completed }"
+          @dblclick="editTodo(todo)"
+        >{{ todo.title }}</div>
+        <input
+          v-else
+          class="todo-item-edit"
+          type="text"
+          v-model="todo.title"
+          @blur="doneEdit(todo)"
+          @keyup.enter="doneEdit(todo)"
+          v-focus
+          @keyup.esc="cancelEdit(todo)"
+        />
+      </div>
+
+      <div class="remove-item" @click="removeTodo(index)">&times;</div>
+    </div>
+    <div class="extra-container">
+      <div>
+        <label>
+          <input type="checkbox" :checked="!anyRemaining" @change="checkAllTodos" /> Check All
+        </label>
+      </div>
+      <div>{{ remaining }} item(s) remaining</div>
+    </div>
+
+    <div class="extra-container">
+      <div>
+        <button :class="{ active: filter == 'all'}" @click="filter = 'all'">All</button>
+        <button :class="{ active: filter == 'active'}" @click="filter = 'active'">Active</button>
+        <button :class="{ active: filter == 'completed'}" @click="filter = 'completed'">Completed</button>
+      </div>
+      <div>
+        <transition name="fade">
+          <button v-if="showClearCompletedButton" @click="clearCompleted">Clear Completed</button>
+        </transition>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "todo-list",
+  data() {
+    return {
+      newTodo: "",
+      idForTodo: 3,
+      beforeEditCache: "",
+      filter: "all",
+      todos: [
+        {
+          id: 1,
+          title: "Finish Vue Screencast",
+          completed: false,
+          editing: false
+        },
+        {
+          id: 2,
+          title: "Take over world",
+          completed: false,
+          editing: false
+        }
+      ]
+    };
+  },
+  directives: {
+    focus: {
+      // directive definition
+      inserted: function(el) {
+        el.focus();
+      }
+    }
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodo.trim().length == 0) {
+        return;
+      }
+      this.todos.push({
+        id: this.idForTodo,
+        title: this.newTodo,
+        completed: false
+      });
+
+      (this.newTodo = ""), this.idForTodo++;
+    },
+    removeTodo(index) {
+      this.todos.splice(index, 1);
+    },
+    editTodo(todo) {
+      this.beforeEditCache = todo.title;
+      todo.editing = true;
+    },
+    doneEdit(todo) {
+      if (todo.title.trim().length == 0) {
+        todo.title = this.beforeEditCache;
+        return;
+      }
+      todo.editing = false;
+      const index = this.todos.findIndex(item => item.id == todo.id);
+      this.todos.splice(index, 1, todo);
+    },
+    cancelEdit(todo) {
+      todo.title = this.beforeEditCache;
+      todo.editing = false;
+    },
+    checkAllTodos() {
+      this.todos.forEach(todo => (todo.completed = event.target.checked));
+    },
+    clearCompleted() {
+      this.todos = this.todos.filter(todo => !todo.completed);
+    }
+  },
+  computed: {
+    remaining() {
+      return this.todos.filter(todo => !todo.completed).length;
+    },
+    anyRemaining() {
+      return this.remaining != 0;
+    },
+    todosFiltered() {
+      if (this.filter == "all") {
+        return this.todos;
+      } else if (this.filter == "active") {
+        return this.todos.filter(todo => !todo.completed);
+      } else if (this.filter == "completed") {
+        return this.todos.filter(todo => todo.completed);
+      }
+
+      return this.todos;
+    },
+    showClearCompletedButton() {
+      return this.todos.filter(todo => todo.completed).length > 0;
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+.todo-input {
+  width: 100%;
+  padding: 10px 18px;
+  font-size: 18px;
+  margin-bottom: 16px;
+
+  &:focus {
+    outline: 0;
+  }
+}
+
+.todo-item {
+  display: flex;
+  margin-bottom: 1rem;
+  align-items: center;
+  justify-content: space-between;
+
+  .remove-item {
+    cursor: pointer;
+    &:hover {
+      color: #000;
+    }
+  }
+
+  .todo-item-left {
+    display: flex;
+    align-items: center;
+  }
+
+  .todo-item-label {
+    padding: 10px;
+    border: 1px solid #fff;
+    margin-left: 12px;
+  }
+
+  .todo-item-edit {
+    font-size: 24px;
+    color: #2c3e50;
+    margin-left: 12px;
+    width: 100%;
+    padding: 10px;
+    border: 1px solid #ccc;
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+
+    &:focus {
+      outline: none;
+    }
+  }
+
+  .completed {
+    text-decoration: line-through;
+    color: #333;
+  }
+
+  div:last-child {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    button {
+      margin-right: 1rem;
+    }
+  }
+}
+
+.extra-container {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 1rem;
+  border-top: 1px solid lightgrey;
+  padding-top: 14px;
+  margin-bottom: 14px;
+}
+
+button {
+  font-size: 14px;
+  background-color: #fff;
+  appearance: none;
+
+  &:hover {
+    background: lightgreen;
+  }
+
+  &:focus {
+    outline: none;
+  }
+}
+
+.active {
+  background: lightgreen;
+}
+
+// CSS transitions
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.4s;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
